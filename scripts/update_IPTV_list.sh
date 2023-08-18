c85d97fb93cb88a826c9f05f1e6e27fd8ed92e8f
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2023-08-18 14:04:56 +0700

    Phimmoi uses wget, phimiptv change -A

--- scripts/update_IPTV_list.sh
+++ scripts/update_IPTV_list.sh
@@ -11,12 +11,12 @@ sed -i '1d' ./playlists/iptvkhangg.m3u
 sed -i '$d' ./playlists/iptvkhangg.m3u
 
 # phimmoi
-curl --compressed https://hqth.me/Xjjeq -o ./playlists/phimmoi.m3u
+wget https://hqth.me/Xjjeq -O ./playlists/phimmoi.m3u
 sed -i "s/  - Hân hạnh mời các bạn tham gia nhóm Zalo : https:\/\/zalo.me\/g\/higkqn442//g" ./playlists/phimmoi.m3u
 sed -i "s/ - Mời bạn tham gia nhóm Zalo : https:\/\/zalo.me\/g\/higkqn442//g" ./playlists/phimmoi.m3u
 
 # Phim IPTV
-curl --compressed -H "Dalvik/2.1.O (Linux; U; Android 10; Device)" http://iptv.pro.vn/phimiptv/ -o ./playlists/PhimIPTV.m3u
+curl --compressed -A "Dalvik/2.1.O (Linux; U; Android 10; Device)" http://iptv.pro.vn/phimiptv/ -o ./playlists/PhimIPTV.m3u
 
 # sn
 wget http://gg.gg/sn-90phut -O ./playlists/sn-90phut.m3u
