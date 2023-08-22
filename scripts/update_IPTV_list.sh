7c13928a1e14f7217b93a0f1c0ff3daadfb1e763
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-08-22 15:25:14 +0700

    add more SN lists reorder iptv lists

--- scripts/update_IPTV_list.sh
+++ scripts/update_IPTV_list.sh
@@ -19,8 +19,11 @@ sed -i "s/ - Mời bạn tham gia nhóm Zalo : https:\/\/zalo.me\/g\/higkqn442//
 curl --compressed -A "Dalvik/2.1.O (Linux; U; Android 10; Device)" http://iptv.pro.vn/phimiptv/ -o ./playlists/PhimIPTV.m3u
 
 # sn
-wget http://gg.gg/sn-90phut -O ./playlists/sn-90phut.m3u
 wget http://gg.gg/sn-thapcam -O ./playlists/sn-thapcam.m3u
+wget http://gg.gg/sn-90phut -O ./playlists/sn-90phut.m3u
+wget http://gg.gg/sn-cakhia -O ./playlists/sn-cakhia.m3u
+wget http://gg.gg/sn-saoke -O ./playlists/sn-saoke.m3u
+wget http://gg.gg/sn-thethao -O ./playlists/sn-thethao.m3u
 
 # coocaa
 wget "https://dl.dropboxusercontent.com/s/fhon6tzc3e20tis/%C3%A2%C2%AD%C3%AF%C2%BF%C2%BD%C3%83%C2%82%C3%AF%C2%BF%C2%BD%C3%83%C2%84" -O ./playlists/coocaa.m3u
