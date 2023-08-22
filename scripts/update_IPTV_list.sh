bf5b0c4d257e10f8125687cf77f129f9bdb0d129
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-08-22 15:34:06 +0700

    add AllMovies list add 90p-atv

--- scripts/update_IPTV_list.sh
+++ scripts/update_IPTV_list.sh
@@ -28,5 +28,8 @@ wget http://gg.gg/sn-thethao -O ./playlists/sn-thethao.m3u
 # coocaa
 wget "https://dl.dropboxusercontent.com/s/fhon6tzc3e20tis/%C3%A2%C2%AD%C3%AF%C2%BF%C2%BD%C3%83%C2%82%C3%AF%C2%BF%C2%BD%C3%83%C2%84" -O ./playlists/coocaa.m3u
 
-# PhapFerrari
+# phaptx5
 wget -U "OTT Navigator/1.6.9.2 (Linux; Android 10; en; 172kbeq)" "http://phaptx5.x10.mx:80/phapferrariVIP2.php" -O ./playlists/phaptx5.m3u
+
+# 90p-atv
+wget http://hoaivnpt.duckdns.org/live-football/live-football.php -O ./playlists/90p-atv.m3u
