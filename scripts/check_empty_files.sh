97614f9cf656cb09a7cc7a5d854165b09fb2539a
Trần Nguyễn Thái Bình <trannguyenthaibinh46@gmail.com>
2023-08-27 14:26:42 +0000

    add check_empty_files.sh

---
+++ scripts/check_empty_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ./playlists
+empty_files=()
+
+for file in *.m3u; do
+    if [ ! -s "$file" ]; then
+        echo -e "$file\t\t: Empty!!!"
+        empty_files+=("$file")
+        check_exit=true
+    else
+        echo -e "$file\t\t: Not empty"
+    fi
+done
+
+echo
+echo "------"
+echo
+
+if [[ -v check_exit ]]; then
+    echo "It's possible to have one or more empty files!!!"
+    echo "${empty_files[@]}"
+    exit 1
+else
+    echo "All files are not empty ^^"
+fi
