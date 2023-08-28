f411dbabb01902f2a13394f935190e3e65869905
Trần Nguyễn Thái Bình <trannguyenthaibinh46@gmail.com>
2023-08-28 14:43:34 +0000

    👷 🙈 ♻️ make playlists functions independent

--- scripts/IPTV_playlists_functions.sh
+++ scripts/IPTV_playlists_functions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# IPTV_playlists_functions.sh
 
 function vthanhtivi() {
     wget http://playlist.vthanhtivi.pw/ -O ./playlists/vthanhtivi.m3u
@@ -39,42 +39,3 @@ function phaptx5() {
 function 90p-atv() {
     wget http://hoaivnpt.duckdns.org/live-football/live-football.php -O ./playlists/90p-atv.m3u
 }
-
-function echo_sep() {
-    echo
-    if [[ -z $1 ]]; then
-        devider="-"
-    else
-        devider=$1
-    fi
-    for ((i = 0; i < devider_length; i++)); do
-        echo -n $devider
-    done
-    echo -e "\n"
-}
-
-rm -rf playlists
-mkdir playlists
-
-devider_length=30
-functions=("vthanhtivi" "iptv_khangg" "phimmoi" "phimiptv" "sn" "coocaa" "phaptx5" "90p-atv")
-current_folder_size=0
-
-echo "UPDATE FOR: ${#functions[@]} playlist(s)"
-echo "${functions[@]}"
-echo
-
-for func in "${functions[@]}"; do
-    padding=$((($devider_length - ${#func}) / 2))
-    echo_sep
-    printf "%-${padding}s%s%-${padding}s\n" "" "$func" ""
-    echo_sep
-    "$func"
-    if [[ "$current_folder_size" -eq "$(du -s | cut -f1)" ]]; then
-        echo_sep
-        echo "RETRY o_O: $func"
-        echo_sep
-        "$func"
-    fi
-    current_folder_size=$(du -s | cut -f1)
-done
